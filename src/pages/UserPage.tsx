d620d8ea9ca26cc13746b44f200ed6a71e90e9f5
Ondrej <536477@muni.cz>
2025-07-01 20:54:15 +0200

    fix: bg

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,14 +1,17 @@
-import { Flex, Title } from '@mantine/core'
+import { Container, Flex, Paper, Title } from '@mantine/core'
 import { UserTable } from '../components/UserTable'
 import { UserForm } from '../components/UserForm'
 
 export default function Users() {
-
   return (
-    <Flex direction="column" gap="md">
-      <Title order={1}>Users</Title>
-      <UserForm />
-      <UserTable />
-    </Flex>
+    <Container size="md" py="xl">
+      <Paper p="xl" radius="md" shadow="md" bg="white">
+        <Flex direction="column" gap="lg">
+          <Title order={1}>Users</Title>
+          <UserForm />
+          <UserTable />
+        </Flex>
+      </Paper>
+    </Container>
   )
 }
