6edb6ed4a13e9af5032bad1fb9bd1934b25a4f52
Ondrej <536477@muni.cz>
2025-07-01 19:22:00 +0200

    feat: display users using Mantine table sort

---
+++ src/components/UserTable.tsx
@@ -0,0 +1,182 @@
+import { useState, useEffect } from 'react';
+import { IconChevronDown, IconChevronUp, IconSearch, IconSelector } from '@tabler/icons-react';
+import {
+  Center,
+  Group,
+  keys,
+  ScrollArea,
+  Table,
+  Text,
+  TextInput,
+  UnstyledButton,
+  Badge,
+} from '@mantine/core';
+import classes from './TableSort.module.css';
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+const fetchUsers = async () => {
+    const res = await axios.get('https://inqool-interview-api.vercel.app/api/users')
+    return res.data
+  }
+
+interface RowData {
+  id: string;
+  name: string;
+  gender: string;
+  banned: boolean;
+}
+
+interface ThProps {
+  children: React.ReactNode;
+  reversed: boolean;
+  sorted: boolean;
+  onSort: () => void;
+}
+
+function Th({ children, reversed, sorted, onSort }: ThProps) {
+  const Icon = sorted ? (reversed ? IconChevronUp : IconChevronDown) : IconSelector;
+  return (
+    <Table.Th className={classes.th}>
+      <UnstyledButton onClick={onSort} className={classes.control}>
+        <Group justify="space-between">
+          <Text fw={500} fz="sm">
+            {children}
+          </Text>
+          <Center className={classes.icon}>
+            <Icon size={16} stroke={1.5} />
+          </Center>
+        </Group>
+      </UnstyledButton>
+    </Table.Th>
+  );
+}
+
+function filterData(data: RowData[], search: string) {
+  const query = search.toLowerCase().trim();
+  return data.filter((item) =>
+    keys(data[1]).some((key) => item[key]?.toString().toLowerCase().includes(query))
+  );
+}
+
+function sortData(
+  data: RowData[],
+  payload: { sortBy: keyof RowData | null; reversed: boolean; search: string }
+) {
+  const { sortBy } = payload;
+
+  if (!sortBy) {
+    return filterData(data, payload.search);
+  }
+
+  return filterData(
+    [...data].sort((a, b) => {
+      if (payload.reversed) {
+        return b[sortBy]?.toString().localeCompare(a[sortBy]?.toString());
+      }
+
+      return a[sortBy]?.toString().localeCompare(b[sortBy]?.toString());
+    }),
+    payload.search
+  );
+}
+
+
+export function UserTable() {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['users'],
+        queryFn: fetchUsers,
+      })
+    
+     
+
+  const [search, setSearch] = useState('');
+  const [sortedData, setSortedData] = useState<RowData[]>([]);
+  const [sortBy, setSortBy] = useState<keyof RowData | null>(null);
+  const [reverseSortDirection, setReverseSortDirection] = useState(false);
+
+  useEffect(() => {
+    if (data) {
+      setSortedData(data);
+    }
+  }, [data]);
+
+  const setSorting = (field: keyof RowData) => {
+    const reversed = field === sortBy ? !reverseSortDirection : false;
+    setReverseSortDirection(reversed);
+    setSortBy(field);
+    setSortedData(sortData(data, { sortBy: field, reversed, search }));
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setSearch(value);
+    setSortedData(sortData(data, { sortBy, reversed: reverseSortDirection, search: value }));
+  };
+
+  const rows = sortedData.map((row) => (
+    <Table.Tr key={row.id}>
+      <Table.Td>{row.name}</Table.Td>
+      <Table.Td>{row.gender}</Table.Td>
+      <Table.Td>
+        {row.banned ? <Badge color="red">Banned</Badge> : <Badge color="green">Active</Badge>}
+      </Table.Td>
+    </Table.Tr>
+  ));
+
+
+  if (isLoading) return <div>Načítavam používateľov...</div>
+  if (error) return <div>Chyba pri načítaní dát</div>
+
+  return (
+    <ScrollArea>
+      <TextInput
+        placeholder="Search by any field"
+        mb="md"
+        leftSection={<IconSearch size={16} stroke={1.5} />}
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <Table horizontalSpacing="md" verticalSpacing="xs" miw={700} layout="fixed">
+        <Table.Tbody>
+          <Table.Tr>
+            <Th
+              sorted={sortBy === 'id'}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting('id')}
+            >
+              ID
+            </Th>
+            <Th
+              sorted={sortBy === 'name'}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting('name')}
+            >
+              Name
+            </Th>
+            <Th
+              sorted={sortBy === 'gender'}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting('gender')}
+            >
+              Gender
+            </Th>
+          </Table.Tr>
+        </Table.Tbody>
+        <Table.Tbody>
+          {rows.length > 0 ? (
+            rows
+          ) : (
+            <Table.Tr>
+              <Table.Td colSpan={Object.keys(data[0]).length}>
+                <Text fw={500} ta="center">
+                  Nothing found
+                </Text>
+              </Table.Td>
+            </Table.Tr>
+          )}
+        </Table.Tbody>
+      </Table>
+    </ScrollArea>
+  );
+}
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,29 +1,16 @@
-// Users.tsx
 
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import { Flex, Title } from '@mantine/core'
+import { UserTable } from '../components/UserTable'
 
-const fetchUsers = async () => {
-  const res = await axios.get('https://inqool-interview-api.vercel.app/api/users')
-  return res.data
-}
+
 
 export default function Users() {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['users'],
-    queryFn: fetchUsers,
-  })
-
-  if (isLoading) return <div>Načítavam používateľov...</div>
-  if (error) return <div>Chyba pri načítaní dát</div>
 
   return (
-    <ul>
-      {data.map((user: any) => (
-        <li key={user.id}>
-          {user.name} ({user.gender}) {user.banned ? '🚫 Banned' : ''}
-        </li>
-      ))}
-    </ul>
+    <Flex direction="column" gap="md">
+      <Title order={1}>Users</Title>
+
+      <UserTable />
+    </Flex>
   )
 }
