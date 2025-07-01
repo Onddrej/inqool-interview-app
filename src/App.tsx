38d8a62e6fbd0ade771a87cc6fae5492701c7c88
Ondrej <536477@muni.cz>
2025-07-01 18:03:35 +0200

    feat: add simple navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import { Button, Flex, Title } from "@mantine/core";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import UsersPage from "./pages/UserPage";
+import AnimalsPage from "./pages/AnimalPage";
 
-function App() {
+function Home() {
+  const navigate = useNavigate();
   return (
     <Flex direction="column" align="center" justify="center" h="100vh">
       <Title order={1}>Do you want to interact with ...</Title>
-      <Flex
-        mih={100}
-        gap="sm"
-        justify="center"
-        align="center"
-        direction="row"
-        wrap="wrap"
-      >
-        <Button variant="filled" size="xl" radius="md">
+      <Flex mih={100} gap="sm" justify="center" align="center" direction="row" wrap="wrap">
+        <Button variant="filled" size="xl" radius="md" onClick={() => navigate("/users")}>
           Users
         </Button>
-        <Button variant="filled" size="xl" radius="md">
+        <Button variant="filled" size="xl" radius="md" onClick={() => navigate("/animals")}>
           Animals
         </Button>
       </Flex>
@@ -23,4 +20,12 @@ function App() {
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/users" element={<UsersPage />} />
+      <Route path="/animals" element={<AnimalsPage />} />
+    </Routes>
+  );
+}
---
+++ src/pages/AnimalPage.tsx
@@ -0,0 +1,3 @@
+export default function AnimalsPage() {
+    return <h2>Animals Page</h2>;
+  }
