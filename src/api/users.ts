549b8a417630d87f6a51cb5a0093fa6a5b14aaec
Ondrej <536477@muni.cz>
2025-07-02 16:41:13 +0200

    refactor: put base api URL to separate file

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { API_BASE_URL } from './config'
 
 export interface RowData {
   id: string;
@@ -8,26 +9,26 @@ export interface RowData {
 }
 
 export const fetchUsers = async () => {
-  const res = await axios.get('https://inqool-interview-api.vercel.app/api/users')
+  const res = await axios.get(`${API_BASE_URL}/users`)
   return res.data
 }
 
 export const createUser = async (user: Omit<RowData, 'id'>) => {
-  const res = await axios.post('https://inqool-interview-api.vercel.app/api/users', user);
+  const res = await axios.post(`${API_BASE_URL}/users`, user);
   return res.data;
 }
 
 export const updateUser = async (id: string, user: Partial<Omit<RowData, 'id'>>) => {
-  const res = await axios.patch(`https://inqool-interview-api.vercel.app/api/users/${id}`, user);
+  const res = await axios.patch(`${API_BASE_URL}/users/${id}`, user);
   return res.data;
 }
 
 export const banUser = async (id: string) => {
-  const res = await axios.patch(`https://inqool-interview-api.vercel.app/api/users/${id}`, { banned: true });
+  const res = await axios.patch(`${API_BASE_URL}/users/${id}`, { banned: true });
   return res.data;
 }
 
 export const unbanUser = async (id: string) => {
-  const res = await axios.patch(`https://inqool-interview-api.vercel.app/api/users/${id}`, { banned: false });
+  const res = await axios.patch(`${API_BASE_URL}/users/${id}`, { banned: false });
   return res.data;
 }
